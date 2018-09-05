6319f517b99f6a255d21d7197b9ab9c75d84ba5c
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-09-05 15:30:29 +0300

    some docker-gcc fixes

--- docker_build/gcc_build.sh
+++ docker_build/gcc_build.sh
@@ -4,19 +4,31 @@ echo "Building with gcc"
 
 set -x -e
 
-echo <<EOF >> install.sh
+rm -rf install.sh || echo "No install file found"
+export PREFIX=/tmp/pheniqs
+export PWD=$(pwd)
+
+cat <<EOF >>install.sh
 #!/usr/bin/env bash
 
 set -x -e
+
+echo "Installing system packages"
+apt-get update -y
+apt-get install -y rsync
+
+echo "Installing miniconda"
+cd /tmp
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b
+bash Miniconda3-latest-Linux-x86_64.sh -b -p /tmp/miniconda3
 
-cd $(pwd)
-
-##Begin pheniqs install
-export PREFIX=/tmp/pheniqs
+echo "Beginning pheniqs install"
+cd $PWD
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-make all PREFIX=${PREFIX}
+export PATH=/tmp/miniconda3/bin/:\$PATH
+
+./tool/ppkg.py -v debug build test/build.json
+make PREFIX=${PREFIX}
 make install PREFIX=${PREFIX}
 
 pheniqs demux --config test/BDGGG/BDGGG_interleave.json --validate --distance
@@ -29,6 +41,8 @@ EOF
 
 chmod 777 install.sh
 
+
 docker run \
+ 	--rm \
 	-it -v $(pwd):$(pwd) gcc:4.9 \
 	$(pwd)/install.sh
