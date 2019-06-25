e8b7ae04116b0b70d250dd6fd533befce35bd5bb
Lior Galanti <lior.galanti@gmail.com>
2019-06-24 22:20:08 -0400

    Update make_example.sh

--- example/H7LT2DSXX/make_example.sh
+++ example/H7LT2DSXX/make_example.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 pheniqs-illumina-api.py basecall illumina/181014_A00534_0024_AH7LT2DSXX
-pheniqs-illumina-api.py core illumina/181014_A00534_0024_AH7LT2DSXX
+pheniqs-illumina-api.py core illumina/181014_A00534_0024_AH7LT2DSXX --no-input-npf
 pheniqs-illumina-api.py multiplex illumina/181014_A00534_0024_AH7LT2DSXX
 pheniqs-illumina-api.py estimate illumina/181014_A00534_0024_AH7LT2DSXX
 pheniqs-illumina-api.py interleave illumina/181014_A00534_0024_AH7LT2DSXX
