acab71047580fddbe27fca6e5c376c4b89fac40c
Lior Galanti <lior.galanti@gmail.com>
2020-07-26 17:44:11 -0400

    Update search.js

--- docs/js/search.js
+++ docs/js/search.js
@@ -35,20 +35,22 @@
   if (searchTerm) {
     document.getElementById('search-box').setAttribute("value", searchTerm);
 
-
+    // Initalize lunr with the fields it will be searching on. I've given title
+    // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
-      this.ref('id')
-      this.field('url')
-      this.field('text')
-      this.field('content')
+      this.ref('id');
+      this.field('title', { boost: 10 });
+      this.field('content');
 
-      for (var key in window.store) { // Add the data to lunr
-        this.add({
-            'id': key,
-            'title': window.store[key].title,
-            'content': window.store[key].content
-        })
-    })
+    });
+
+    for (var key in window.store) { // Add the data to lunr
+      idx.add({
+        'id': key,
+        'title': window.store[key].title,
+        'content': window.store[key].content
+      });
+    }
 
     var results = idx.search(searchTerm); // Get lunr to perform a search
     displaySearchResults(results, window.store); // We'll write this in the next section
